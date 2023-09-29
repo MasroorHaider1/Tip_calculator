389a3031f633b2fcb79b758eddc5cd0e5cb17d25
MasroorHaider1 <146111560+MasroorHaider1@users.noreply.github.com>
2023-09-29 11:36:56 -0400

    Update Main.java
    This is tip

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,34 @@
-import java.util.Scanner;
+
+CODE:import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello, welcome to my bill program.");
         Scanner scan = new Scanner(System.in);
+      
         System.out.println("Please enter an amount in dollars and cents: ");
         // defined bill
         double OriginalBill= scan.nextDouble();
+      
         System.out.println("Please enter the Tip amount as a whole number: ");
         // defined Tip
         double Tip= scan.nextInt();
+      
         System.out.println("Please enter the  amount of people as a whole number: ");
         // defined People
         double People= scan.nextInt();
 
-
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
         //total tip amount
-        double TipAmount= OriginalBill * Tip;
-        System.out.println("The Total Tip amount is:"+(Tip/100));
+        double TipAmount= OriginalBill * (Tip/100);
+        System.out.println("The Total Tip amount is:"+decimalFormat.format(TipAmount));
         //total bill
         double TotalBill= OriginalBill+TipAmount;
-        System.out.println("The total bill is"+TotalBill);
+        System.out.println("The total bill is: "+decimalFormat.format(TotalBill));
         //tip per person
         double TipPerPerson=TipAmount/People;
-        System.out.println("The tip per person is"+ TipPerPerson);
+        System.out.println("The tip per person is: "+ decimalFormat.format(TipPerPerson));
         double BillPerPerson=TotalBill/People;
-        System.out.println("The bill per person is"+ BillPerPerson);
+        System.out.println("The bill per person is: "+ decimalFormat.format(BillPerPerson));
     }
 }
